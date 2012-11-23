7b4ffc78539a02d894c0548d65fd2050103c22c9
hezhengfang <hezhengfang08@163.com>
2012-11-23 09:40:57 +0800

    "day05"

---
+++ hello.m
@@ -0,0 +1,10 @@
+ #import <Foundation/Foundation.h>
+ int main (int argc, const char * argv[])
+ {    
+	// NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+       NSLog(@"Programming is fun!");
+     
+      //[pool release];
+      return 0;
+ }
+
